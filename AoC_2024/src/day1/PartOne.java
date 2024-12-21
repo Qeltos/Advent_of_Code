f0f550a3122d2af6d87f6fecb50e96795ad1f0cb
Mikael Wedin <mikael_wedin@hotmail.com>
2024-12-21 18:19:09 +0100

    debugging git error

--- AoC_2024/src/day1/PartOne.java
+++ AoC_2024/src/day1/PartOne.java
@@ -5,6 +5,7 @@ public class PartOne {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("New git commit");
+		System.out.println("Third commit");
 	}
 
 }
